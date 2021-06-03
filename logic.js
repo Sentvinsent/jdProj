e7a5273ef25053521b410ab02f9ba6d2d45b1f0d
Sentvinsent <alexfilimonchuk@gmail.com>
2021-06-03 20:04:17 +0300

    async/1 time dom

--- logic.js
+++ logic.js
@@ -2,37 +2,35 @@
 
 const key = '5ea363cd1fe377e3ae1dcc973693a928';
 const baseURL = 'https://api.themoviedb.org/3/';
-const srchResDivVar = document.getElementById('srchResDiv');
-
 
 //takes the input value and search DB with it
-let search = function() {
+let search = async function() {
+    const srchResDivVar = document.getElementById('srchResDiv');
     let title = document.getElementById("srchInp").value;
     srchResDivVar.innerHTML = '';
     let url = `${baseURL}search/movie?api_key=${key}&query=${title}`;
+    let result = await fetch(url);
+    let data = await result.json();
 
-    fetch(url)
-        .then((result) => {
-            return result.json();
-        })
-        .then((data) => {
-            let filmsArray = data.results;
-            let filmsData = filmsArray.map(obj => {
-                let fimsDataObj = {
-                    'title': obj.title,
-                    'rating': obj.vote_average,
-                    'description': obj.overview,
-                    'image': 'https://image.tmdb.org/t/p/w500/' + obj.poster_path
-                };
-                return fimsDataObj;
-            })
-            populateSearchData(filmsData);
-        })
-
+    let filmsArray = data.results;
+    let filmsData = filmsArray.map(obj => {
+        let fimsDataObj = {
+            'title': obj.title,
+            'rating': obj.vote_average,
+            'description': obj.overview,
+            'image': 'https://image.tmdb.org/t/p/w500/' + obj.poster_path
+        };
+        return fimsDataObj;
+    })
+    populateSearchData(filmsData);
 }
 
 //populates the result data into the result Div
 function populateSearchData(filmsData) {
+    const srchResDivVar = document.getElementById('srchResDiv');
+    const newD = document.createElement('div');
+    newD.className = "srchResDivClass";
+    newD.setAttribute('id', 'srchResDiv');
     filmsData.forEach(element => {
         const newH = document.createElement('h2');
         newH.innerText = element.title;
@@ -45,35 +43,33 @@ function populateSearchData(filmsData) {
         newT.className = 'insideResEl';
         const line = document.createElement('hr');
         line.className = 'line';
-        srchResDivVar.appendChild(newH);
-        srchResDivVar.appendChild(newI);
-        srchResDivVar.appendChild(newT);
-        srchResDivVar.appendChild(line);
+        newD.appendChild(newH);
+        newD.appendChild(newI);
+        newD.appendChild(newT);
+        newD.appendChild(line);
     });
+    srchResDivVar.replaceWith(newD);
 }
 
 //get to Films
-let topTen = function() {
-        let url = `${baseURL}movie/top_rated?api_key=${key}&language=en-US&page=1`;
+let topTen = async function() {
+    let url = `${baseURL}movie/top_rated?api_key=${key}&language=en-US&page=1`;
 
-        fetch(url)
-            .then((result) => {
-                return result.json();
-            })
-            .then((data) => {
-                let filmsArray = data.results;
-                let filmsData = filmsArray.slice(0, 10).map(obj => {
-                    let fimsDataObj = {
-                        'title': obj.title,
-                        'rating': obj.vote_average,
-                        'description': obj.overview,
-                        'image': 'https://image.tmdb.org/t/p/w500/' + obj.poster_path
-                    };
-                    return fimsDataObj;
-                })
-                populateSearchData(filmsData);
-            })
-    }
-    //event listeners
+    let result = await fetch(url);
+    let data = await result.json();
+    let filmsArray = data.results;
+    let filmsData = filmsArray.slice(0, 10).map(obj => {
+        let fimsDataObj = {
+            'title': obj.title,
+            'rating': obj.vote_average,
+            'description': obj.overview,
+            'image': 'https://image.tmdb.org/t/p/w500/' + obj.poster_path
+        };
+        return fimsDataObj;
+    })
+    populateSearchData(filmsData);
+}
+
+//event listeners
 document.getElementById('srchBtn').addEventListener('click', search);
 document.getElementById('topTenButton').addEventListener('click', topTen);
